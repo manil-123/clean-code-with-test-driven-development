ad8a18b7fb4c03542a526b32fac8be32df7f38f6
Manil Pun <mpun@himalayantechies.com>
2023-02-07 23:00:55 +0545

    necessary package added.

---
+++ lib/features/login/presentation/pages/login_screen.dart
@@ -0,0 +1,68 @@
+import 'package:ecom_clean_code/core/widgets/custom_text_field.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String route = '/loginScreen';
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Screen'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomTextFormField(
+                controller: _usernameController,
+                labelText: "Username",
+                keyboardType: TextInputType.name,
+                validator: (String? username) {
+                  if (username!.isEmpty) {
+                    return "User name cannot be empty";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              CustomTextFormField(
+                controller: _passwordController,
+                labelText: "Password",
+                obsecure: true,
+                validator: (String? username) {
+                  if (username!.isEmpty) {
+                    return "Password cannot be empty";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Login'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:ecom_clean_code/features/login/presentation/pages/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const String route = '/';
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToNextScreen();
+  }
+
+  void _navigateToNextScreen() {
+    Future.delayed(const Duration(milliseconds: 2000), () {
+      Navigator.of(context).pushReplacementNamed(LoginScreen.route);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Splash Screen'),
+      ),
+    );
+  }
+}
