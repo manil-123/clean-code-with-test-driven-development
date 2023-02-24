7e98bda839a1f7c100d1e6d0f8da1f42a346f3c0
Manil Pun <mpun@himalayantechies.com>
2023-02-24 23:02:43 +0545

    Login repository completed.

---
+++ lib/features/login/data/repository/login_repository_impl.dart
@@ -0,0 +1,24 @@
+import 'package:ecom_clean_code/core/constants/constants.dart';
+import 'package:ecom_clean_code/core/error/exceptions.dart';
+import 'package:ecom_clean_code/features/login/data/datasource/login_remote_data_source.dart';
+import 'package:ecom_clean_code/features/login/domain/entities/login_model.dart';
+import 'package:ecom_clean_code/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ecom_clean_code/features/login/domain/repository/login_repository.dart';
+
+class LoginRepositoryImpl implements LoginRepository {
+  final LoginRemoteDataSource loginRemoteDataSource;
+
+  LoginRepositoryImpl({required this.loginRemoteDataSource});
+  @override
+  Future<Either<Failure, LoginModel>> login(
+      String username, String password) async {
+    try {
+      final loginResponse =
+          await loginRemoteDataSource.loginUser(username, password);
+      return Right(loginResponse);
+    } on ServerException {
+      return Left(Failure(ErrorMessage.serverFailureMessage));
+    }
+  }
+}
---
+++ test/features/login/data/repository/login_repository_impl_test.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+
+import 'package:dartz/dartz.dart';
+import 'package:ecom_clean_code/core/error/exceptions.dart';
+import 'package:ecom_clean_code/core/error/failures.dart';
+import 'package:ecom_clean_code/features/login/data/datasource/login_remote_data_source.dart';
+import 'package:ecom_clean_code/features/login/data/model/login_data_model.dart';
+import 'package:ecom_clean_code/features/login/data/repository/login_repository_impl.dart';
+import 'package:ecom_clean_code/features/login/domain/entities/login_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'login_repository_impl_test.mocks.dart';
+
+@GenerateMocks([LoginRemoteDataSource])
+void main() {
+  late LoginRepositoryImpl loginRepositoryImpl;
+  late MockLoginRemoteDataSource mockLoginRemoteDataSource;
+
+  setUp(() {
+    mockLoginRemoteDataSource = MockLoginRemoteDataSource();
+    loginRepositoryImpl =
+        LoginRepositoryImpl(loginRemoteDataSource: mockLoginRemoteDataSource);
+  });
+
+  const username = "username";
+  const password = "password";
+
+  final testLoginModel = LoginDataModel(token: "testToken");
+
+  group("login repository implementation test", () {
+    test(
+      "should login when the call to remote data source is successful",
+      () async {
+        //arrange
+        when(mockLoginRemoteDataSource.loginUser(username, password))
+            .thenAnswer((_) async => testLoginModel);
+        //act
+        final result = await loginRepositoryImpl.login(username, password);
+        //assert
+        verify(mockLoginRemoteDataSource.loginUser(username, password));
+        expect(result, Right(testLoginModel));
+      },
+    );
+
+    test(
+      'should return Failure when an exception thrown',
+      () async {
+        // arrange
+        when(mockLoginRemoteDataSource.loginUser(any, any))
+            .thenThrow(ServerException('Server Error'));
+
+        // act
+        final result = await loginRepositoryImpl.login(username, password);
+
+        // assert
+        verify(mockLoginRemoteDataSource.loginUser(username, password));
+        verifyNoMoreInteractions(mockLoginRemoteDataSource);
+        expect(result, equals(Left(Failure('Server Error'))));
+      },
+    );
+  });
+}
