0df75d5a5881a38b46e19ef616cbd08e772d6d59
Manil Pun <punmanil3@gmail.com>
2023-03-11 00:20:44 +0545

    Categories data layer

---
+++ lib/features/categories/data/datasource/category_remote_data_source.dart
@@ -0,0 +1,14 @@
+import 'package:ecom_clean_code/core/constants/constants.dart';
+import 'package:ecom_clean_code/core/data/base_remote_data_source.dart';
+import 'package:ecom_clean_code/core/data/json_decoder.dart';
+import 'package:ecom_clean_code/features/categories/data/model/category_data_model.dart';
+import 'package:http/src/response.dart';
+
+class CategoryRemoteDataSource extends BaseRemoteDataSourceImpl {
+  CategoryRemoteDataSource(super.client, super.sharedPreferences);
+
+  Future<List<CategoryDataModel>> getCategoriesList() async {
+    final response = await performGetRequest(ApiEndpoints.categoriesUrl);
+    final data = jsonDecodeAndHandleException(response);
+  }
+}
---
+++ lib/features/categories/data/model/category_data_model.dart
@@ -0,0 +1,5 @@
+import 'package:ecom_clean_code/features/categories/domain/entity/category.dart';
+
+class CategoryDataModel extends Category {
+  const CategoryDataModel({required super.categoryName});
+}
